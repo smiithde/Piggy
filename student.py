45e6e717637e2b6ff2c225d11e18975564053f89
smiithde <70531311+smiithde@users.noreply.github.com>
2020-09-28 11:35:20 -0400

    Making the robot work faster

--- student.py
+++ student.py
@@ -101,9 +101,7 @@ class Piggy(PiggyParent):
    
     def forward_shuffle(self):
         """ Walk Forward"""
-        for x in range(2):
-            self.fwd()
-            time.sleep(.01)
+        for x in range(4):
             self.fwd()
             time.sleep(.01)
             self.stop()
