1f3ce5f08958fb5ceac2f28f5eeaacf6072d3dbc
smiithde <70531311+smiithde@users.noreply.github.com>
2020-10-06 10:52:08 -0400

    Changing back to original

--- student.py
+++ student.py
@@ -17,7 +17,7 @@ class Piggy(PiggyParent):
         ''' 
         MAGIC NUMBERS <-- where we hard-code our settings
         '''
-        self.LEFT_DEFAULT = 83
+        self.LEFT_DEFAULT = 80
         self.RIGHT_DEFAULT = 85
         self.SAFE_DISTANCE = 300
         self.CLOSE_DISTANCE = 150
