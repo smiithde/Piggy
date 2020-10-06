b977292f244db44e5c281855a7003edd4ccff02c
smiithde <70531311+smiithde@users.noreply.github.com>
2020-10-06 10:45:47 -0400

    Changing servo to center

--- student.py
+++ student.py
@@ -21,7 +21,7 @@ class Piggy(PiggyParent):
         self.RIGHT_DEFAULT = 80
         self.SAFE_DISTANCE = 300
         self.CLOSE_DISTANCE = 150
-        self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
+        self.MIDPOINT = 1525  # what servo command (1000-2000) is straight forward for your bot?
         self.set_motor_power(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
         self.load_defaults()
         
