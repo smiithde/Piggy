fd099dd338b9afbd77ad6ac71f983d4bd4cd11b2
smiithde <70531311+smiithde@users.noreply.github.com>
2020-10-07 10:48:34 -0400

    Adding quick check

--- student.py
+++ student.py
@@ -207,6 +207,17 @@ class Piggy(PiggyParent):
         """Does a 360 scan and returns the number of obstacles it sees"""
         pass
 
+    def quick_check(self):
+        """ Moves the servo to three angles and performs a distance check """
+        #Write a better docustring
+        #loop three times and move the servo
+        for ang in range(self.MIDPOINT - 100, self.MIDPOINT + 101, 100):
+            self.servo(ang)
+            time.sleep(.1)
+            if self.read_distance() <self.SAFE_DISTANCE:
+                return False
+        # if the three-part check didn't freak out 
+        return True
 
     def nav(self):
         """  Auto-pilot program """
@@ -217,14 +228,13 @@ class Piggy(PiggyParent):
         # TODO: build self.quick_check() that does a fast, 3-part check instead of read_distance
         self.fwd()
         while True:
-            if self.read_distance() < self.CLOSE_DISTANCE: 
+            if not self.quick_check(): 
                 self.stop()
                 print("OH NO!!!! Something is in my way!!")
                 self.turn_by_deg(90)
                 time.sleep(.1)
             else:
                 self.fwd()
-            time.sleep(.01)
         self.stop()
         # TODO: scan so we can decide left or right
         # TODO: average the right side of the scan dict
