c1159847c9f3eacc4b3a7164d887cc3a609288de
smiithde <70531311+smiithde@users.noreply.github.com>
2020-10-09 10:42:37 -0400

    adding to safe distance

--- student.py
+++ student.py
@@ -203,20 +203,31 @@ class Piggy(PiggyParent):
         for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 10):
             self.servo(angle)
             self.scan_data[angle] = self.read_distance()
+        # sort the scan data for easier analysis
+        self.scan_data = OrderedDict(sorted(self.scan_data.items()))
 
     def obstacle_count(self):
         """Does a 360 scan and returns the number of obstacles it sees"""
         # do a scan of the area in front of the robot
         self.scan()
-        # sort the scan data for easier analysis
-        self.scan_data = OrderedDict(sorted(self.scan_data.items()))
+        # FIGURE OUT HOW MANY OBSTACLES THERE WERE
+        see_an_object = False
+        count = 0
+        for x in range(4):
+            for angle in self.scan_data:
+                dist = self.scan_data[angle]
+                if dist < self.SAFE_DISTANCE and not see_an_object:
+                    see_an_object = True
+                    count += 1
+                    print("~~~~ I SEE SOMETHING!!! ~~~~")
+                elif dist > self.SAFE_DISTANCE and see_an_object:
+                    see_an_object = False
+                    print ("I guess the object ended")
 
-        # print the results
-        for angle in self.scan_data:
-            dist = self.scan_data[angle]
-            print ("ANGLE: %d | DIST: %d" % (angle, dist))
+                print ("ANGLE: %d | DIST: %d" % (angle, dist))
+            print("\nI saw %d objects" % count)
             
-    
+
     def quick_check(self):
         """ Moves the servo to three angles and performs a distance check """
         #Write a better docustring
