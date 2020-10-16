860f72d434153d286dcbdfa21b05378e4cd1551d
smiithde <70531311+smiithde@users.noreply.github.com>
2020-10-16 15:03:26 -0400

    allowing it to actually do the ()

--- student.py
+++ student.py
@@ -315,9 +315,9 @@ class Piggy(PiggyParent):
                 if turn_count > 3 and turn_count % 5 == 0:
                     self.turn_to_deg(exit_angle)
                 elif 'l' in self.right_or_left():
-                    self.turn_left_until_clear
+                    self.turn_left_until_clear()
                 else:  
-                    self.turn_right_until_clear
+                    self.turn_right_until_clear()
                 turn_count += 1
 
             else:
