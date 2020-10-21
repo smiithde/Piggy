9ba79cc75b55b64687f0c2e5fdbb52e5a547a8e2
smiithde <70531311+smiithde@users.noreply.github.com>
2020-10-21 11:07:33 -0400

    giving it back to original

--- student.py
+++ student.py
@@ -316,7 +316,7 @@ class Piggy(PiggyParent):
                 self.back()
                 time.sleep(1)
                 self.stop()
-                if turn_count > 3 and turn_count % 4 == 0:
+                if turn_count > 4 and turn_count % 5 == 0:
                     self.turn_to_deg(exit_angle)
                 elif 'l' in self.right_or_left():
                     self.turn_left_until_clear()
